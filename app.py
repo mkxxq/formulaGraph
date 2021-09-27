6b89c08ae48ba5e1a30bf3244960da0c600c642e
sun qiang <sunqiang@zhuozhuo.io>
2021-09-27 15:31:06 +0800

    add cache

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+import hashlib
+import os.path
+import sys
+import threading
+
 import sympy.core
 from flask import Flask, render_template, request, flash, Markup, jsonify
 
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, SubmitField, BooleanField, PasswordField, IntegerField, TextField,\
+from wtforms import StringField, SubmitField, BooleanField, PasswordField, IntegerField, TextField, \
     FormField, SelectField, FieldList
 from wtforms.validators import DataRequired, Length
 from wtforms.fields import *
@@ -11,8 +16,9 @@ from wtforms.fields import *
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sympy.parsing.latex import parse_latex
-from sympy.plotting import plot,plot_implicit
+from sympy.plotting import plot, plot_implicit
 import matplotlib
+
 matplotlib.use("TKAgg")
 
 app = Flask(__name__)
@@ -35,10 +41,39 @@ app.config['BOOTSTRAP_TABLE_NEW_TITLE'] = 'Create'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
+cache = {}
+lock = threading.Lock()
+
+
+def write_cache(img_path):
+    lock.acquire()
+    try:
+        cache[img_path] = True
+    finally:
+        lock.release()
+
+
+def read_cache(img_path):
+    lock.acquire()
+    try:
+        if img_path in cache:
+            return os.path.exists(img_path)
+    finally:
+        lock.release()
+        return False
+
+
+def gen_img_path(l):
+    if not os.path.exists('static'):
+        os.mkdir('static')
+    md5 = hashlib.md5()
+    md5.update(l.encode())
+    fname = "{}.jpg".format(md5.hexdigest())
+    return os.path.join('static',fname)
 
 
 class LatexForm(FlaskForm):
-    inputVal = StringField('Latex',validators=[DataRequired(),Length(1,100)])
+    inputVal = StringField('Latex', validators=[DataRequired(), Length(1, 100)])
     submit = SubmitField()
 
 
@@ -57,38 +92,45 @@ def before_first_request_func():
     db.create_all()
     for i in range(20):
         m = Message(
-            text=f'Test message {i+1}',
-            author=f'Author {i+1}',
-            category=f'Category {i+1}',
-            create_time=4321*(i+1)
-            )
+            text=f'Test message {i + 1}',
+            author=f'Author {i + 1}',
+            category=f'Category {i + 1}',
+            create_time=4321 * (i + 1)
+        )
         if i % 4:
             m.draft = True
         db.session.add(m)
     db.session.commit()
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    latexForm = LatexForm()
-    formulaLatex = latexForm.inputVal.data
+    latex_form = LatexForm()
+    formula_latex = latex_form.inputVal.data
     formula = None
-    if formulaLatex:
+    img = ''
+    if formula_latex:
+        formula_latex = formula_latex.strip()
+        img = gen_img_path(formula_latex)
+        if read_cache(img):
+            return render_template('graph.html', latex_form=latex_form, img=img)
         try:
-            formula = parse_latex(formulaLatex)
+            formula = parse_latex(formula_latex)
         except:
             formula = None
-    img = ''
-    p = None
     if formula is not None:
-        if isinstance(formula,sympy.Equality):
-            p = plot_implicit(formula,show=False)
+        p = None
+        if isinstance(formula, sympy.Equality):
+            p = plot_implicit(formula, show=False)
         else:
-            p = plot(formula,show=False)
+            try:
+                p = plot(formula, show=False)
+            except:
+                pass
         if p is not None:
-            p.save("static/a.jpg")
-            img = 'static/a.jpg'
-    return render_template('graph.html',latex_form=latexForm,img=img)
+            p.save(img)
+            write_cache(img)
+    return render_template('graph.html', latex_form=latex_form, img=img)
 
 
 if __name__ == '__main__':
